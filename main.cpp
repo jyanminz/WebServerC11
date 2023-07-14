0247556829b799d79150893322a6462220b48bf9
jyanminz <486643493@qq.com>
2023-07-14 09:09:23 +0800

    add string vector to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 int main(){
-	cout << "hello world!" << endl;
+	vector<string> strs = {
+		"test",
+		"git",
+		"push",
+		"commit"
+	};
+
+	cout << "hello world" << endl;
+
+	for (const auto & val : strs) {
+		cout << val << endl;
+	}
+
 	return 0;
 }
