5e5c19bc8a8cd1f545f67577ee00ec86305a633d
jyanminz <486643493@qq.com>
2023-07-15 10:04:01 +0800

    add buffer

---
+++ code/buffer/buffer.cpp
@@ -0,0 +1,111 @@
+#include "buffer.h"
+
+Buffer::Buffer(int initBufferSize) : readPos_(0), writePos_(0), buffer_(initBufferSize) {}
+
+size_t Buffer::ReadableBytes() const {
+	return writePos_ - readPos_;
+}
+
+size_t Buffer::WritableBytes() const {
+	return buffer_.size() - writePos_;
+}
+
+size_t Buffer::PrependableBytes() const {
+	return readPos_;
+}
+
+const char * Buffer::Peek() const {
+	return BeginPtr_() + readPos_;
+}
+
+void Buffer::Retrieve(size_t len) {
+	assert(len <= ReadableBytes());
+	readPos_ += len;
+}
+
+void Buffer::RetrieveUntil(const char * end) {
+	assert(Peek() <= end);
+	Retrieve(end - Peek());
+}
+
+void Buffer::RetrieveAll() {
+	bzero(&buffer_[0], buffer_.size());
+	readPos_ = 0;
+	writePos_ = 0;
+}
+
+std::string Buffer::RetrieveAllToStr() {
+	std::string str(Peek(), ReadableBytes());
+	RetrieveAll();
+	return str;
+}
+
+const char * Buffer::BeginWriteConst() const {
+	return BeginPtr_() + writePos_;
+}
+
+char * Buffer::BeginWrite() {
+	return BeginPtr_() + writePos_;
+}
+
+void Buffer::HasWritten(size_t len) {
+	writePos_ += len;
+}
+
+void Buffer::Append(const std::string& str) {
+	Append(str.data(), str.length());
+}
+
+void Buffer::Append(const void * data, size_t len) {
+	assert(data);
+	Append(static_cast<const char*>(data), len);
+}
+
+void Buffer::Append(const char * str, size_t len) {
+	assert(str);
+	EnsureWriteable(len);
+	std::copy(str, str+len, BeginWrite());
+	HasWritten(len);
+}
+
+void Buffer::Append(const Buffer& buff) {
+	Append(buff.Peek(), buff.ReadableBytes());
+}
+
+void Buffer::EnsureWriteable(size_t len) {
+	if (WritableBytes() < len) {
+		MakeSpace_(len);
+	}
+	assert(WritableBytes() >= len);
+}
+
+ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
+	char buff[65535];
+	struct iovec iov[2];
+
+	const size_t writable = WritableBytes();
+
+	iov[0].iov_base = BeginPtr_() + writePos_;
+	iov[0].iov_len = writable;
+	iov[1].iov_base = buff;
+	iov[1].iov_len = sizeof(buff);
+
+	const ssize_t len = readv(fd, iov, 2);
+
+	if (len < 0) {
+		// error
+		*saveErrno = errno;
+	}
+
+	else if (static_cast<size_t>(len) <= writable) {
+		// 全部写入到buffer中
+		writePos_ += len;
+	}
+
+	else {
+		// 部分写入到了buff中，buffer写满
+		writePos_ = buffer_.size();
+		Append(buff, len - writable);
+	}
+	return len;
+}
