bd0f703e3f658f063743464f39b3950710eb84cb
jyanminz <486643493@qq.com>
2023-07-17 17:31:52 +0800

    httprequest finishied

--- code/http/httprequest.cpp
+++ code/http/httprequest.cpp
@@ -213,3 +213,70 @@ std::string HttpRequest::GetPost(const char* key) const {
 	return "";
 }
 
+bool HttpRequest::UserVerify(const string& name, const string& pwd, bool isLogin) {
+	if ("" == name || "" == pwd) 
+		return false;
+	LOG_INFO("VERIFY NAME: %s, PASSWD: %s", name.c_str(), pwd.c_str());
+	MYSQL* sql;
+	SqlConnRAII(&sql, SqlConnPool::Instance());
+	assert(sql);
+
+	bool flag = false;
+	unsigned int j = 0;
+	char order[256] = {0};
+	MYSQL_FIELD& fields = nullptr;
+	MYSQL_RES * res = nullptr;
+
+	if (!isLogin) 
+		flag = true;
+
+	snprintf(order, 256, "SELECT username, password FROM user WHERE usernam='%s' LIMIT 1", name.c_str());
+	LOG_DEBUG("%s", order);
+
+	if (mysql_query(sql, order)) {
+		mysql_free_result(res);
+		return false;
+	}
+
+	res = mysql_store_result(sql);
+
+	j = mysql_num_fields(res);
+
+	fields = mysql_fetch_fields(res);
+
+	while(MYSQL_ROW row = mysql_fetch_row(rew)) {
+		LOG_DEBUG("MYSQL ROW: %s %s", row[0], row[1]);
+		string password(row[1]);
+
+		if (isLogin){
+			if (password == pwd) {
+				flag =true;
+			}
+			else {
+				flag = false;
+				LOG_DEBUG("PWD ERROR");
+			}
+		}
+		else {
+			flag =false;
+			LOG_DEBUG("user used");
+		}
+	}
+	mysql_free_result(res);
+
+	if (!isLogin && flag == true) {
+		LOG_DEBUG("register");
+		bzero(order, 256);
+		snprintf(order, 256, "INSERT INTO user(username, password) VALUES('%S', '%S')", name.c_str(), pwd.c_str());
+		LOG_DEBUG("%s", order);
+		if (mysql_query(sql, order)) {
+			LOG_DEBUG("Insert Error");
+			flag = false;
+		}
+		flag = true;
+	}
+	SqlConnPool::Instance()->FreeConn(sql);
+	LOG_DEBUG("UserVerify success");
+	return flag;
+}
+
